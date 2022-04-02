cd508bb60059d6cdc7b7f84f71dacfb3a77d1e96
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2022-04-03 02:16:12 +0300

    Create main-page

--- ControlRemote/Web/ClientApp/ControlRemoteApp/src/app/app.module.ts
+++ ControlRemote/Web/ClientApp/ControlRemoteApp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: MainPageComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
