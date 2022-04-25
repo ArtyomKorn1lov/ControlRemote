2d67796c69ee1ad1d88102de38225611513dbcd0
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2022-04-25 16:37:05 +0300

    Add base authentifications methods

---
+++ ControlRemote/Web/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using Domain.Entity;
+using Infrastructure;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Web.Dto;
+using Web.DtoConverter;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private ControlRemoteDbContext _controlRemoteDbContext;
+
+        public AccountController(ControlRemoteDbContext controlRemoteDbContext)
+        {
+            _controlRemoteDbContext = controlRemoteDbContext;
+        }
+
+        [HttpPost("login")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Manager manager = await _controlRemoteDbContext.Set<Manager>().FirstOrDefaultAsync(u => u.Login == model.Login && u.Password == model.Password);
+                UserDto user = UserDtoConverter.ConvertToUserDto(manager);
+                if (user != null)
+                {
+                    await Authenticate(model.Login); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        [HttpPost("register")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Manager manager = await _controlRemoteDbContext.Set<Manager>().FirstOrDefaultAsync(u => u.Login == model.Login);
+                UserDto user = UserDtoConverter.ConvertToUserDto(manager);
+                if (user == null)
+                {
+                    // добавляем пользователя в бд
+                    Manager new_user = UserDtoConverter.ConvertToUserEntiy(user);
+                    _controlRemoteDbContext.Set<Manager>().Add(new_user);
+                    await _controlRemoteDbContext.SaveChangesAsync();
+
+                    await Authenticate(model.Login); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ ControlRemote/Web/DtoConverter/UserDtoConverter.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Dto;
+
+namespace Web.DtoConverter
+{
+    public static class UserDtoConverter
+    {
+        public static UserDto ConvertToUserDto(Manager manager)
+        {
+            if(manager == null)
+            {
+                return null;
+            }
+            return new UserDto
+            {
+                Id = manager.Id,
+                Name = manager.Name,
+                Login = manager.Login,
+                Password = manager.Password
+            };
+        }
+        public static Manager ConvertToUserEntiy(UserDto userDto)
+        {
+            if(userDto == null)
+            {
+                return null;
+            }
+            return new Manager
+            {
+                Id = userDto.Id,
+                Name = userDto.Name,
+                Login = userDto.Login,
+                Password = userDto.Password,
+                Employers = null,
+            };
+        }
+    }
+}
